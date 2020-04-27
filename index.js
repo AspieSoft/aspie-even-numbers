ca6d4a160c1f22215b954562c282f3a6dae6ed96
AspieSoft <aspiesoftweb@gmail.com>
2020-04-27 09:04:45 -0400

    Update

--- index.js
+++ index.js
@@ -1,24 +1,32 @@
 // In God We Trust
 
-function checkEven(number){
+function checkEven(number, useMultiple){
 	number = Number(number);
-	return number === setEvenNumber(number);
+	return number === setEvenNumber(number, useMultiple);
 }
 
-function setEven(number){
-	number = setEvenNumber(number);
+function setEven(number, useMultiple){
+	number = setEvenNumber(number, useMultiple);
 	let loops = 1000;
-	while(number !== setEvenNumber(number) && loops-- > 0){
-		number = setEvenNumber(number);
+	while(number !== setEvenNumber(number, useMultiple) && loops-- > 0){
+		number = setEvenNumber(number, useMultiple);
 		if(loops < 0){break;}
 	}
-	if(number !== setEvenNumber(number)){
-		number = setEvenNumber(Math.round(number));
+	if(number !== setEvenNumber(number, useMultiple)){
+		number = setEvenNumber(Math.round(number), useMultiple);
 	}return number;
 }
 
+function isEven(number){
+	return isNumberType(number, [0, 2, 4, 6, 8]);
+}
 
-function setEvenNumber(number){
+function is5Multiple(number){
+	return isNumberType(number, [0, 5]);
+}
+
+
+function setEvenNumber(number, useMultiple){
 	number = number.toString().split('.');
 	if(!number[0] || number[0].trim() === ''){number[0] = undefined;}
 	if(!number[1] || number[1].trim() === ''){number[1] = undefined;}
@@ -31,7 +39,7 @@ function setEvenNumber(number){
 				if(!rounded[1] || rounded[1].trim() === ''){rounded[1] = '0';}
 				number[1][i-1] = rounded[0]; number[1][i] = rounded[1];
 			}
-			number[1][i] = fixNumber(number[1][i]);
+			number[1][i] = fixNumber(number[1][i], useMultiple);
 			if(number[1][i] === '10'){
 				number[1][i] = '0';
 				if(i !== 0){
@@ -40,7 +48,12 @@ function setEvenNumber(number){
 			}
 		}number[1] = number[1].join('');
 	}
-	if(number[0]){let lNum = fixNumber(number[0].slice(-1)); number[0] = number[0].slice(0, -1)+lNum;}
+	if(number[0]){
+		let lNum = fixNumber(number[0].slice(-1), useMultiple);
+		if(Number(lNum) >= 10){
+			number[0] = (Number(number[0].slice(0, -1)+'0') + Number(lNum)).toString();
+		}else{number[0] = number[0].slice(0, -1)+lNum;}
+	}
 	if(number[0] || number[1]){
 		if(!number[0]){number[0] = '0';}
 		if(!number[1]){number[1] = '0';}
@@ -48,14 +61,50 @@ function setEvenNumber(number){
 	}return NaN;
 }
 
-function fixNumber(number){
+function fixNumber(number, useMultiple = 'both'){
 	number = Number(number);
-	if([1, 3, 6].includes(number)){
-		number--;
-	}else if([4, 7, 9].includes(number)){
-		number++;
+	if(useMultiple == 2){
+		if([1, 3, 5, 7, 9].includes(number)){
+			number++;
+		}
+	}else if(useMultiple == 5){
+		if(number < 3){
+			number = 0;
+		}else if(number <= 7){
+			number = 5;
+		}else{
+			number = 10;
+		}
+	}else{
+		if([1, 3, 6].includes(number)){
+			number--;
+		}else if([4, 7, 9].includes(number)){
+			number++;
+		}
 	}return number.toString();
 }
 
+function isNumberType(number, allowedNumbers){
+	number = number.toString().split('.');
+	if(!Array.isArray(allowedNumbers)){allowedNumbers = [allowedNumbers.toString()];}
+	for(let i = 0; i < allowedNumbers.length; i++){allowedNumbers[i] = allowedNumbers[i].toString();}
+	if(!number[0] || number[0].trim() === ''){number[0] = undefined;}
+	if(!number[1] || number[1].trim() === ''){number[1] = undefined;}
+	if(number[1]){
+		number[1] = number[1].split('');
+		for(let i = number[1].length-1; i >= 0; i--){
+			if(!allowedNumbers.includes(number[1][i])){
+				return false;
+			}
+		}
+	}
+	if(number[0]){
+		let lNum = number[0].slice(-1);
+		if(!allowedNumbers.includes(lNum)){
+			return false;
+		}
+	}return true;
+}
 
-module.exports = {check: checkEven, set: setEven};
+
+module.exports = {check: checkEven, set: setEven, isEven, is5Multiple};
--- readme.md
+++ readme.md
@@ -31,10 +31,43 @@ const aspieEven = require('aspie-even-numbers');
 ### Usage
 
 ```js
+// check if a number will be changed by .set()
 aspieEven.check(2); // output: true
 aspieEven.check(3.07); // output: false
+aspieEven.check(4); // output: false (because 4 is set to 5)
+aspieEven.check(5); // output: true (because 5 is a multiple of 5)
 
+// set a number to the correct number (even or multiple of 5)
 aspieEven.set(17); // output: 18
 aspieEven.set(4.39); // output 5.5
 aspieEven.set(4.89); // output 5
+aspieEven.set(29); // output: 30
+
+// check only if a number is even
+aspieEven.isEven(4); // output: true
+aspieEven.is5Multiple(5); // output: false
+
+// check only if a number is a multiple of 5
+aspieEven.is5Multiple(2); // output: false
+aspieEven.is5Multiple(5); // output: true
+aspieEven.is5Multiple(10); // output: true
+
+// .checkEven() and .setEven() have another parameter (useMultiple: default = 'both')
+
+// when useMultiple = 2
+aspieEven.check(4, 2); // output: true
+aspieEven.check(5, 2); // output: false
+
+aspieEven.set(1, 2); // output: 2
+aspieEven.set(3, 2); // output: 4
+
+// when useMultiple = 5
+aspieEven.check(4, 5); // output: false
+aspieEven.check(5, 5); // output: true
+
+aspieEven.set(2, 5); // output: 0
+aspieEven.set(3, 5); // output: 5
+aspieEven.set(7, 5); // output: 5
+aspieEven.set(8, 5); // output: 10 (corrects to adding to number in front)
+
 ```
