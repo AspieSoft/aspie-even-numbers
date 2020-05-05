644e4d51c316b158a2e1d27ddbd08225a95163c3
AspieSoft <aspiesoftweb@gmail.com>
2020-05-05 12:52:35 -0400

    Update

--- cli.js
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env node
-
-// Grab provided args
-const [,,...args] = process.argv;
-
-console.log(`Test (for cli commands): ${args}`);
--- index.js
+++ index.js
@@ -1,15 +1,4 @@
 // In God We Trust
 
-const app = requireOr(['./src/index.min.js', './src/index.js']);
-
-function requireOr(list){
-	if(!Array.isArray(list)){list = [list];} list.push(undefined);
-	for(let i = 0; i < list.length; i++){
-		let listType = typeof list[i];
-		if(list[i] && listType === 'string'){
-			try{return require(list[i]);}catch(e){}
-		}else if(['boolean', 'number', 'undefined'].includes(listType) || list[i] === null){return list[i];}
-	}return undefined;
-}
-
+const app = require('./src/index.min');
 module.exports = app;
