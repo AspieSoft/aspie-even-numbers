5d1a6368500c15aac30966dbd91711a332deb9d4
AspieSoft <aspiesoftweb@gmail.com>
2020-05-28 23:33:25 -0400

    Updated

--- index.js
+++ index.js
@@ -1,10 +1,4 @@
 // In God We Trust
 
 const aspieEven = require('./main.min');
-
-module.exports = {
-	check: aspieEven.check,
-	set: aspieEven.set,
-	isEven: aspieEven.isEven,
-	is5Multiple: aspieEven.is5Multiple,
-};
+module.exports = aspieEven;
